546f3d88357f55680e51282a7fbd90f8abb09827
Ryan G. James <ryangregoryjames@gmail.com>
2020-01-11 15:49:23 -0800

    switch to github actions

--- lattices/__init__.py
+++ lattices/__init__.py
@@ -1,7 +1,8 @@
 # -*- coding: utf-8 -*-
 
 """
-Lattices is a package for the construction of lattices from nodes and an ordering relation.
+Lattices is a package for the construction of lattices from a set of nodes and
+an ordering relation.
 """
 
 __version__ = "0.3.4"
