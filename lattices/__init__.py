3efdd7ac0defe7b0d06519569f219246a9fbeb10
Ryan G. James <ryangregoryjames@gmail.com>
2020-01-19 10:14:48 -0800

    bump version

--- lattices/__init__.py
+++ lattices/__init__.py
@@ -3,6 +3,6 @@ Lattices is a package for the construction of lattices from a set of nodes and
 an ordering relation.
 """
 
-__version__ = "0.3.4"
+__version__ = "0.3.5"
 
 from .lattice import Lattice
